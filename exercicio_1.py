cddf60b1f6a80bf7fa842a5f7731b704da420583
LetiLSP <163673548+LetiLSP@users.noreply.github.com>
2024-04-01 20:46:20 -0300

    Update exercicio_1.py

--- exercicio_1.py
+++ exercicio_1.py
@@ -17,3 +17,10 @@ Digite um número:
 
 Dica: Lembre do comando de resto da divisão inteira!
 """
+
+
+numero = int(input("Digite um número: "))
+if (numero % 2) == 0:
+    print("Par")
+else:
+    print("Ímpar")
